38d30e97bfa195fc9a182d940c7560c24f091d0b
kalettuce <jiangz34@uw.edu.com>
2019-11-05 19:17:06 -0800

    Formatting of md file, adding nc_extract.c

--- src/EXECUTABLE_GUIDE.md
+++ src/EXECUTABLE_GUIDE.md
@@ -1,29 +1,29 @@
-*ALL FUNCTIONS*
+#ALL FUNCTIONS
 
-    ***dtoul***
-    Usage: dtoul_b infilename outfilename
+***dtoul***
+Usage: dtoul\_b infilename outfilename
 
-    Reads an input binary file full of 8-byte double values, and convert them
-    into unsigned long representation of the same values.
+Reads an input binary file full of 8-byte double values, and convert them
+into unsigned long representation of the same values.
 
-    ***to_epoch***
-    Usage: to_epoch infilename outfilename
+***to_epoch***
+Usage: to\_epoch infilename outfilename
 
-    Reads an input binary file full of 8 byte double values that represents
-    seconds since 1900-01-01 00:00 and convert them to seconds since Epoch.
+Reads an input binary file full of 8 byte double values that represents
+seconds since 1900-01-01 00:00 and convert them to seconds since Epoch.
 
-    ***analyze***
-    Usage: temp_analyze LOW HIGH threshold ul_ts_filename infilename..
+***analyze***
+Usage: temp\_analyze LOW HIGH threshold ul\_ts\_filename infilename..
 
-    Reads from one or more binary data files(with data encoded as doubles) and a corresponding
-    timestamps file, and prints to the output each and every intervals of time where data values 
-    are between user-defined upper and lower bounds. Only intervals with length greater than 
-    user-defined threshold value will be display, but all intervals are going to be included in the
-    summary section as part of the total statistics
+Reads from one or more binary data files(with data encoded as doubles) and a corresponding
+timestamps file, and prints to the output each and every intervals of time where data values 
+are between user-defined upper and lower bounds. Only intervals with length greater than 
+user-defined threshold value will be display, but all intervals are going to be included in the
+summary section as part of the total statistics
 
-    ***len_cmp***
-    Usage: len_cmp binary_file_1 binary_file_2
-    Reads from and compares the length of 2 files in terms of number of 8-bytes, and prints to the
-    output the count for each of the files
-    ####(DON'T USE; JUST GO WITH du --apparent-size -B 8; I DIDN'T KNOW ABOUT THAT WHEN I WROTE IT)
+***len_cmp***
+Usage: len\_cmp binary\_file\_1 binary\_file\_2
+Reads from and compares the length of 2 files in terms of number of 8-bytes, and prints to the
+output the count for each of the files
+####(DON'T USE; JUST GO WITH 'du --apparent-size -B 8'; I DIDN'T KNOW ABOUT THAT WHEN I WROTE IT)
 
---
+++ src/nc_extract.c
@@ -0,0 +1,96 @@
+#include <netcdf.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define ARG_ERR 1
+#define FILE_ERR 2
+#define NC_ERR -1
+
+/* handles nc errors by printing out error message and quits */
+void handle_err(int status);
+
+int main(int argc, char **argv) {
+
+    // argument check
+    if (argc != 3) {
+        fprintf(stderr, "Usage: nc_extract input.nc output_filename");
+        exit(ARG_ERR);
+    }
+
+    int ncid;
+    int status;
+    // check and open the input file in read-only mode
+    if ((status = nc_open(argv[1], NC_NOWRITE, &ncid))) {
+        handle_err(status);
+    }
+
+    /************* Extraction of miscellaneous data ***************/
+    // containers declaration
+    int varid, dimid, ndims;
+    size_t dimlen;
+    // get the varid
+    if ((status = nc_inq_varid(ncid, "time", &varid))) {
+        handle_err(status);
+    }
+
+    // get the size of the var
+    if ((status = nc_inq_varndims(ncid, varid, &ndims))) {
+        handle_err(status);
+    }
+
+    // get the dimid of the desired dimension
+    if ((status = nc_inq_dimid(ncid, "obs", &dimid))) {
+        handle_err(status);
+    }
+
+    // get the length of the dimension
+    if ((status = nc_inq_dimlen(ncid, dimid, &dimlen))) {
+        handle_err(status);
+    }
+    /****************** End of misc data *********************/
+    /****************** Extraction of main bulk of data ********************/
+    /****************** Reading data into memory *****************/
+    uint64_t *var = malloc(dimlen * sizeof(uint64_t));
+    size_t index[5] = {0};
+    if ((status = nc_get_var_ulonglong(ncid, varid, var))) {
+    // if ((status = nc_get_var1_double(ncid, varid, index, var))) {
+        handle_err(status);
+    }
+
+    /***************** Writing data to output *****************/
+    // set up the file stream, append if file exists
+    FILE *output;
+    if (access(argv[2], F_OK)) { // when the file doesn't exist
+        output = fopen(argv[2], "w");
+    } else if (access(argv[2], W_OK)) { // when the user doesn't have writing permission
+        fprintf(stderr, "Permission denied.\n");
+        exit(FILE_ERR);
+    } else {
+        output = fopen(argv[2], "a");
+    }
+
+    // a second check in case when the file doesn't exist and user doesn't have permission
+    // to write in the target directory
+    if (!output) {
+        fprintf(stderr, "Permission denied, or the output path might point to a directory\n");
+        exit(FILE_ERR);
+    }
+
+    fwrite(var, dimlen, sizeof(double), output);
+    /******************* End Extraction ************************/
+
+    // housekeeping
+    fclose(output);
+    nc_close(ncid);
+    free(var);
+
+    return 0;
+}
+
+// handles nc errors by printing out error message and exits
+void handle_err(int status) {
+    fprintf(stderr,"%s\n", nc_strerror(status));
+    exit(NC_ERR);
+}
